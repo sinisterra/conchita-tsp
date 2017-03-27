2106c556323f853c0d759048c1b9031edf95997e
Santiago Sinisterra <santiago.sinisterra.s@gmail.com>
2017-03-27 09:44:59 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import { links, cities, encodeAsLinks, generateInitialPopulation, fitness, runGeneration } from './CoffeeMule'
+import { values, shuffle, range, indexOf, map, max, min, get, forEach } from 'lodash'
+import * as d3 from 'd3'
+
+import './App.css'
+
+class App extends Component {
+
+  state = {
+    solutionIndex: 0,
+    generations: 30,
+    history: []
+  }
+
+  componentDidMount() {
+    const population = generateInitialPopulation(10)
+    this.setState({
+      initialPopulation: population,
+      currentPopulation: population
+    })
+  }
+
+  _viewNextSolution = () => {
+    this.setState({
+      solutionIndex: min([100, this.state.solutionIndex + 1])
+    })
+  }
+
+  _viewPrevSolution = () => {
+    this.setState({
+      solutionIndex: max([0, this.state.solutionIndex - 1])
+    })
+  }
+
+
+  _runGeneration = () => {
+    this.setState({
+      // history: [...this.state.history, this.state.currentPopulation],
+      currentPopulation: runGeneration(this.state.currentPopulation)
+    })
+  }
+
+  _autoRun = () => {
+    // let history = [...this.state.history]
+    let currentPopulation = {...this.state.currentPopulation}
+    forEach(range(this.state.generations), (i) => {
+      currentPopulation = runGeneration(currentPopulation)
+      // history.push(currentPopulation)
+    })
+    this.setState({currentPopulation})
+  }
+
+  render() {
+    const route = get(this.state.currentPopulation, 0) || []
+    const displayedRoute = encodeAsLinks(route)
+
+    const lineDrawer = d3.line()
+      .curve(d3.curveMonotoneX)
+      .x(d => d.x1)
+      .y(d => d.y1)
+
+    return (
+      <div>
+        <div>
+          <button onClick={ this._viewPrevSolution }>
+            { `<< Previo` }
+          </button>
+          <span>Fitness: { fitness(route) }</span>
+          <span>GENERATION {this.state.history.length}</span>
+          <button onClick={ this._viewNextSolution }>
+            { `Siguiente >>` }
+          </button>
+          <button onClick={ this._runGeneration }>
+            { `NEXT GENERATION` }
+          </button>
+           <button onClick={ this._autoRun }>
+            { `AUTORUN` }
+          </button>
+          <input type="number" value={this.state.generations} onChange={({target: {name, value}}) => {
+            this.setState({[name]: value})
+          }} name="generations"/>
+        </div>
+        <svg
+          width={ 960 }
+          height={ 960 }>
+          { values(links).map(({d, label, m1, m2, ...dimensions}) => {
+            
+              const isInRoute = indexOf(displayedRoute, label) !== -1
+            
+              return (
+              isInRoute ? <g
+                       className="link"
+                       opacity={1}>
+                       <path
+                         fill={ 'none' }
+                         stroke={ isInRoute ? 'black' : 'black' }
+                         d={ lineDrawer([{
+                               x1: dimensions.x1,
+                               y1: dimensions.y1
+                             }, {
+                               x1: dimensions.x2,
+                               y1: dimensions.y2
+                             }]) } />
+                       <rect />
+                       <text
+                         textAnchor={ 'middle' }
+                         x={ m1 + 10 }
+                         y={ m2 + 2 }
+                         fill={ isInRoute ? 'black' : 'red' }
+                         fontSize={ 12 }>
+                         { d }
+                       </text>
+                     </g> : null
+              )
+            }) }
+          { cities.map(({city, x, y}) => {
+              const isStartingPoint = route[0] === city
+              const isEndingPoint = route[route.length - 1] === city
+            
+              return (
+                <g transform={ `translate(${x},${y})` }>
+                  <circle
+                    r={ isStartingPoint || isEndingPoint ? 20 : 10 }
+                    fill={ isStartingPoint ? 'green' : isEndingPoint ? 'red' : 'black' } />
+                  <text
+                    fill={ 'white' }
+                    textAnchor={ 'middle' }
+                    x={ 0 }
+                    y={ 5 }>
+                    { city }
+                  </text>
+                </g>
+              )
+            }) }
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/CoffeeMule.js
@@ -0,0 +1,200 @@
+import { sample, difference, fill, forEach, sampleSize, random, range, map, min, sum, groupBy, mapValues, pop, sortBy, shuffle, get, reverse, slice, zip, chunk, flatten, max } from 'lodash'
+import _ from 'lodash'
+import 'lodash.combinations'
+
+const CITY_COUNT = 50
+const POPULATION_SIZE = 50
+const ELITISM_PERCENTAGE = 0.2
+const W = 960
+const H = 960
+
+const generateCities = (cities) => {
+  const generateCoordinate = (max) => random(0, max)
+
+  return map(range(cities), city => {
+    const x = 20 + (W * (city / cities))
+    const y = Math.abs(( H - 60 ) * Math.sin(30 * city))
+    return ({
+      city,
+      x,
+      y
+    })
+  })
+}
+
+const distance = (c1, c2) => {
+  const keys = map(['x', 'y'], (k, i) => {
+    const coordinates = map([c1, c2], c => c[k])
+    return Math.abs((coordinates[1] - coordinates[0]))
+  })
+
+  return Math.round(Math.sqrt(sum(keys)))
+}
+
+const midpoint = (c1, c2) => {
+  const keys = map(['x', 'y'], (k, i) => {
+    const coordinates = map([c1, c2], c => c[k])
+    return (coordinates[1] + coordinates[0]) / 2
+  })
+
+  const [m1, m2] = keys
+
+  return {
+    m1,
+    m2
+  }
+}
+
+export const cities = generateCities(CITY_COUNT)
+export const links = map(_.combinations(cities, 2), pair => {
+  const [c1, c2] = pair
+  return {
+    label: `${c1.city},${c2.city}`,
+    x1: c1.x,
+    x2: c2.x,
+    y1: c1.y,
+    y2: c2.y,
+    d: distance(c1, c2),
+    ...midpoint(c1, c2)
+  }
+})
+
+const linksByLabel = mapValues(groupBy(links, 'label'), v => v[0])
+
+export const encodeAsLinks = (vector) => {
+
+  let s = map(vector, (v, i) => {
+
+    const first = vector[i]
+    const second = vector[i + 1]
+    if (second === undefined) {
+      return null
+    }
+    const [n1, n2] = sortBy([first, second])
+
+    return `${n1},${n2}`
+
+  })
+  s.pop()
+
+  return s
+}
+
+export const run = () => {
+
+}
+
+let list = []
+
+export const runGeneration = (population) => {
+
+  const evaluatedPopulation = map(population, chromosome => ({
+    chromosome,
+    fitness: fitness(chromosome)
+  }))
+  const sortedPopulation = sortBy(evaluatedPopulation, ({fitness}) => fitness)
+
+  const selectedPopulation = eliteSelector(sortedPopulation)
+  const crossedPopulation = crossPopulation(selectedPopulation)
+
+
+  const refillPopulation = [...map(selectedPopulation, ({chromosome}) => chromosome), ...crossedPopulation, ...generateInitialPopulation(POPULATION_SIZE - crossedPopulation.length - selectedPopulation.length)]
+
+
+  let s = sortBy(refillPopulation, c => fitness(c))
+  const diff = Math.abs(fitness(s[0]) - fitness(population[0]))
+  list.push(diff)
+
+  console.log(list.length, {
+    diff,
+    f: fitness(s[0]),
+    ind: s[0]
+  })
+
+  // console.log('running')
+
+  return s
+}
+
+const eliteSelector = (sortedPopulation) => {
+  const pct = Math.round(sortedPopulation.length * ELITISM_PERCENTAGE)
+  return slice(sortedPopulation, 0, max([2, pct % 2 == 0 ? pct : pct + 1]))
+}
+
+const crossPopulation = (pop) => {
+
+  const shuffledPop = shuffle(pop)
+  const dividedInPairs = chunk(shuffledPop, 2)
+  const mating = dividedInPairs.map(([p1, p2]) => crossover(p1, p2))
+
+  return flatten(mating)
+
+}
+
+export const generateInitialPopulation = (size = POPULATION_SIZE) => {
+  const generateIndividual = () => {
+    return shuffle(range(CITY_COUNT))
+  }
+  return sortBy(range(size).map(generateIndividual), i => fitness(i))
+}
+
+const mutation = (chromosome) => {
+  if (Math.random() < 0.2) {
+
+    const onePoint = sampleSize(range(CITY_COUNT), sample(range(0, CITY_COUNT, 2)))
+    let mutated = [...chromosome]
+    forEach(chunk(onePoint, 2), ([point1, point2]) => {
+
+      let tmp = mutated[point1]
+      mutated[point1] = mutated[point2]
+      mutated[point2] = tmp
+    })
+    return mutated
+  }
+  return chromosome
+}
+
+const crossover = (p1, p2) => {
+
+  const getASon = (parent1, parent2) => {
+
+    const halfwayPoint = Math.floor(parent1.chromosome.length / 2)
+    const initialPoint = random(0, halfwayPoint)
+    const endPoint = random(halfwayPoint + 1, parent1.chromosome.length - 1)
+
+    let parentSection = slice(parent1.chromosome, initialPoint, endPoint)
+
+    let son = map(fill(Array(CITY_COUNT), null), (e, i) => {
+      return i >= initialPoint && i < endPoint ? parent1.chromosome[i] : null
+    })
+
+    const numbersToAdd = difference(parent2.chromosome, parentSection)
+
+    son = map(son, s => {
+      if (s === null) {
+        return numbersToAdd.pop()
+      }
+      else return s
+    })
+
+    return son
+  }
+
+  const o1 = getASon(p1, p2)
+  const o2 = getASon(p2, p1)
+
+
+
+  return map([o1, o2], mutation)
+}
+
+
+export const fitness = (chromosome) => {
+  const linksFromVector = encodeAsLinks(chromosome)
+  return sum(map(linksFromVector, l => {
+    if (get(linksByLabel, l) === undefined) {
+      debugger
+    }
+    return get(linksByLabel, l).d
+  }))
+}
